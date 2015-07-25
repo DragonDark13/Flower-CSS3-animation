dfa167225d54c22b0c70ee6bac10fa5e15bad773
Aleks <alekz.zak@mail.ru>
2015-07-25 21:27:29 +0400

    Flower Js

---
+++ js/MyScript.js
@@ -0,0 +1,13 @@
+$.fx.speeds._default = 1000;
+
+$(document).ready(function() { 
+
+$('.Flower').click(function(){
+	$(this).toggleClass("Flower--active");
+	
+	
+});
+
+	/*$('.Flower').addClass("Flower--active");*/
+
+}); //Конец ready	
